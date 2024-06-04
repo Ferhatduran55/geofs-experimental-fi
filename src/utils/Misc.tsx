dee3b7636a139fc7673a84c0dc68f53e705ae3bf
Ferhat Duran <42141771+Ferhatduran55@users.noreply.github.com>
2024-06-04 16:54:04 +0300

    feat: Add getObjectFromPath function for retrieving nested object properties

---
+++ src/utils/Misc.tsx
@@ -0,0 +1,11 @@
+export function getObjectFromPath(path: string): object {
+  const parts: any = path.split(".");
+  let obj = unsafeWindow;
+  for (let part of parts) {
+    obj = obj[part];
+    if (obj === undefined) {
+      throw new Error(`Path ${path} does not exist`);
+    }
+  }
+  return obj;
+}
